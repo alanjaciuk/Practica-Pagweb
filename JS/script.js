cd9033754532af881e53db07fcddae8a29d0a68f
Alan Jaciuk <aguitas06@gmail.com>
2026-03-05 20:10:08 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,25 @@
 //Carrusel
 
 const imagenes = [{
-    img:'../Imagenes/K1nG_at_the_FNCS_2025_Global_Championship.jpg',
+    img:'Practica-Pagweb\Imagenes\K1nG_at_the_FNCS_2025_Global_Championship.jpg',
     titulo:"Jugadores Profesionales",
     desc: "Todo sobre los ultimos tops y rendimiento de tus jugadores Profesionales favoritos"},
-    {img: '../Imagenes/cooper.jpg',
+    {img: 'Practica-Pagweb\Imagenes\cooper.jpg',
         titulo:"Jugadores Profesionales",
     desc: "Todo sobre los ultimos tops y rendimiento de tus jugadores Profesionales favoritos"},
-    {img: '../Imagenes/fncs2025.jpg',
+    {img: 'Practica-Pagweb\Imagenes\fncs2025.jpg',
         titulo: "Todo sobre torneos oficiales",
         desc: "Informacion sobre fechas, clasificaciones y finales de los torneos mas emocionantes"
     },
-    {img: '../Imagenes/fncs2024.jpg',
+    {img: 'Practica-Pagweb\Imagenes\fncs2024.jpg',
         titulo: "Todo sobre torneos oficiales",
         desc: "Informacion sobre fechas, clasificaciones y finales de los torneos mas emocionantes"
     },
-    {img: '../Imagenes/actualizaciones1.jpg',
+    {img: 'Practica-Pagweb\Imagenes\actualizaciones1.jpg',
         titulo: "Actualizaciones y novedades",
         desc: "Informaremos sobre posibles y nuevas actualizaciones o eventos del juego"
     },
-    {img: '../Imagenes/actualizaciones2.jpg',
+    {img: 'Practica-Pagweb\Imagenes\actualizaciones2.jpg',
         titulo: "Actualizaciones y novedades",
         desc: "Informaremos sobre posibles y nuevas actualizaciones o eventos del juego"
     }]
